afce08a24b2f098024e4276001b20b345feb74c4
chetanturtlemint <chetan.gslab@turtlemint.com>
2023-03-01 14:57:06 +0530

    my folders

---
+++ limit-service/src/main/java/com/microservices/limitservice/LimitServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.limitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class LimitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitServiceApplication.class, args);
+	}
+
+}
